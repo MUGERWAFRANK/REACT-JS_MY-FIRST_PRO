f84f70ab267a2cbc41e27c879e310dc1e3ba63d0
frank <fsimple47@gmail.com>
2023-08-28 04:13:58 +0300

    pushing to github

---
+++ first-name_c3/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import UserPortfolio from './componentes/UserPortfolio';
+
+
+function App() {
+  return (
+    <UserPortfolio />
+  );
+}
+
+export default App;
---
+++ first-name_c3/src/componentes/UserPortfolio.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const UserPortfolio = () => {
+  return (
+    <div className="portfolio">
+      <h1>Full Name</h1>
+      <p>MUGERWA FRANK</p>
+
+      <h2>Educational Background</h2>
+      <p>DIPLOMA COMPUTER SCIENCE</p>
+
+      <h2>Professional Background</h2>
+      <p>software engineer</p>
+
+      <h2>Skills</h2>
+      <ul>
+        <li>Networking</li>
+        <li>Engineer</li>
+        <li>mainatiner</li>
+        {/* Add more skills as needed */}
+      </ul>
+    </div>
+  );
+}
+
+export default UserPortfolio;
+
+
+
